90e0b1d499d1bd03a3797caee9aa5a889d9c8ed3
Vinh Nguyen <nhquocvinh@gmail.com>
2018-04-22 11:58:22 +0700

    adding spectre.css and prettier

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import App from './App';
+
+import 'spectre.css/dist/spectre.min.css';
+import 'spectre.css/dist/spectre-exp.min.css';
+import 'spectre.css/dist/spectre-icons.min.css';
+
+import './index.css';
+
+ReactDOM.render(<App />, document.getElementById('root'));
